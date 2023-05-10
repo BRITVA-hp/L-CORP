322f71962321862d375a33ddc5296c8b32c5dabf
BRITVA-hp <borbritvahp@gmail.com>
2023-05-10 14:36:00 +0400

    cases

--- js/script.js
+++ js/script.js
@@ -24,6 +24,58 @@ document.addEventListener("DOMContentLoaded", () => {
     //     console.log("Resource conscious resize callback!");
     // });
 
+    // функция для модалки
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scarollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scarollWidth;
+    }
+
+    let scrollWidth = calcScroll();
+
+    function modal(modal, modalActiveClass, triggers, modalClose) {
+        const triggers_ = document.querySelectorAll(triggers),
+            modal_ = document.querySelector(modal),
+            modalClose_ = document.querySelector(modalClose);
+
+        if (triggers_.length > 0) {
+            triggers_.forEach(item => {
+                item.addEventListener('click', (e) => {
+                    e.preventDefault()
+                    modal_.classList.add(modalActiveClass);
+                    document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${scrollWidth}px`;
+                });
+            });
+
+            modalClose_.addEventListener('click', () => {
+                modal_.classList.remove(modalActiveClass);
+                document.body.style.overflow = '';
+                document.body.style.marginRight = '0px';
+            });
+
+            modal_.addEventListener('click', (e) => {
+                if (e.target.classList.contains('modal__container')) {
+                    modal_.classList.remove(modalActiveClass);
+                    document.body.style.overflow = '';
+                    document.body.style.marginRight = '0px';
+                }
+            });
+        }
+    }
+
+    modal('.modal-main', 'modal--active', '[data-modal]', '.modal-main__close');
+
     const burger = document.querySelector('.header__burger')
     const menu = document.querySelector('.menu')
     const flash = document.querySelector('.menu__flash')
